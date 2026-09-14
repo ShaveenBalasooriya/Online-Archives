20769b9957a0b15ade73235e9a4e2fbfac070b00
Shaveen Balasooriya <shaveen.balasooriya@ascentic.se>
2026-09-14 11:55:32 +0530

    [refactor]: Not Found image
    Simplified the image name for the not found image.
    
    Signed-off-by: Shaveen Balasooriya <shaveen.balasooriya@ascentic.se>

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,6 +1,3 @@
 abstract final class AppAssets {
-  static const bookNotFound =
-      'assets/images/mineralogy-abstract-concept-vector-illustration-rocks-'
-      'minerals-applied-geology-mineralogy-degree-petrology-research-fossils-'
-      'microscopic-study-soil-composition-abstract-metaphor.png';
+  static const bookNotFound = 'assets/images/not_found.png';
 }
